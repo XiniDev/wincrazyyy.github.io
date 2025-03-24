2316b96cf680f5784c2d3e070c397a0a9bab31c2
Anthony Leung <anthonyleung07@gmail.com>
2025-03-24 18:00:11 +0000

    update

--- deploy.bat
+++ deploy.bat
@@ -2,10 +2,6 @@
 REM build and generate sitemap
 call npm run deploy
 
-REM ensure sitemap.xml won't cause merge conflicts
-git config merge.ours.driver true
-echo public/sitemap.xml merge=ours>>.gitattributes
-
 REM stage all changes (including sitemap)
 git add .
 
@@ -13,7 +9,7 @@ REM pull latest from source/master
 git pull source master
 
 REM commit your changes
-git commit -m "update" || echo "No changes to commit."
+git commit -m "update"
 
 REM push to remote master
 git push -u source master
