17d17de6efe9a2e3853e484331e2a6d3ed97da74
wincrazyyy <144009297+wincrazyyy@users.noreply.github.com>
2025-03-25 01:58:36 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,6 @@ cd "$(dirname "$0")"
 # build and generate sitemap
 npm run deploy
 
-# ensure sitemap.xml won't cause merge conflicts
-git config merge.ours.driver true
-echo "public/sitemap.xml merge=ours" >> .gitattributes
-
 # stage all changes (including sitemap)
 git add .
 
